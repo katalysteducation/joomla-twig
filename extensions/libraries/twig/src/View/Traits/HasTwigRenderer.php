31fe60b6742a824938aeb81f6eb3f97beed7d282
Tomek Stach <tomasz.stach@gmail.com>
2022-09-26 12:45:40 +0200

    Fix get component name in the getOptionFromPrefix

--- extensions/libraries/twig/src/View/Traits/HasTwigRenderer.php
+++ extensions/libraries/twig/src/View/Traits/HasTwigRenderer.php
@@ -18,87 +18,78 @@ use Phproberto\Joomla\Twig\Twig;
  *
  * @since  1.2.0
  */
-trait HasTwigRenderer
-{
-	/**
-	 * Component option.
-	 *
-	 * @var  string
-	 */
-	protected $option;
+trait HasTwigRenderer {
+  /**
+   * Component option.
+   *
+   * @var  string
+   */
+  protected $option;
 
-	/**
-	 * Get the data that will be sent to renderer.
-	 *
-	 * @return  array
-	 */
-	abstract protected function getLayoutData();
+  /**
+   * Get the data that will be sent to renderer.
+   *
+   * @return  array
+   */
+  abstract protected function getLayoutData();
 
-	/**
-	 * Get this component option.
-	 *
-	 * @return  string
-	 */
-	public function getOption()
-	{
-		if (null === $this->option)
-		{
-			$this->option = $this->getOptionFromPrefix();
-		}
+  /**
+   * Get this component option.
+   *
+   * @return  string
+   */
+  public function getOption() {
+    if (null === $this->option) {
+      $this->option = $this->getOptionFromPrefix();
+    }
 
-		return $this->option;
-	}
+    return $this->option;
+  }
 
-	/**
-	 * Get the component from the prefix. Ex.: ContentViewArticle will return com_content
-	 *
-	 * @return  string
-	 */
-	protected function getOptionFromPrefix()
-	{
-		$class = get_class($this);
+  /**
+   * Get the component from the prefix. Ex.: ContentViewArticle will return com_content
+   *
+   * @return  string
+   */
+  protected function getOptionFromPrefix() {
+    $class = get_class($this);
 
-		if (false !== strpos($class, '\\'))
-		{
-			$name = array_filter(explode('\\', strstr($class, 'View', true)));
-			$name = strtolower(end($name));
-		}
-		else
-		{
-			$name = strtolower(strstr($class, 'View', true));
-		}
+    if (false !== strpos($class, '\\')) {
+      $name = array_filter(explode('\\', strstr($class, 'Site', true)));
+      $name = strtolower(end($name));
+    } else {
+      $name = strtolower(strstr($class, 'View', true));
+    }
 
-		return 'com_' . strtolower($name);
-	}
+    return 'com_' . strtolower($name);
+  }
 
-	/**
-	 * Load a template file -- first look in the templates folder for an override
-	 *
-	 * @param   string  $tpl  The name of the template source file; automatically searches the template paths and compiles as needed.
-	 *
-	 * @return  string  The output of the the template script.
-	 *
-	 * @throws  \Exception
-	 */
-	public function loadTemplate($tpl = null)
-	{
-		$layout = $this->getLayout();
-		$tpl = $tpl ? $layout . '_' . $tpl : $layout;
+  /**
+   * Load a template file -- first look in the templates folder for an override
+   *
+   * @param   string  $tpl  The name of the template source file; automatically searches the template paths and compiles as needed.
+   *
+   * @return  string  The output of the the template script.
+   *
+   * @throws  \Exception
+   */
+  public function loadTemplate($tpl = null) {
+    $layout = $this->getLayout();
+    $tpl = $tpl ? $layout . '_' . $tpl : $layout;
 
-		$renderer = Twig::instance();
+    $renderer = Twig::instance();
 
-		$data = $this->getLayoutData();
-		$prefix = '@component/' . $this->getOption() . '/' . $this->getName();
+    $data = $this->getLayoutData();
+    $prefix = '@component/' . $this->getOption() . '/' . $this->getName();
 
-		$name = $prefix . '/' . $tpl . '.html.twig';
+    $name = $prefix . '/' . $tpl . '.html.twig';
 
-		if ($renderer->environment()->getLoader()->exists($name))
-		{
-			return $renderer->render($name, $data);
-		}
+    if ($renderer->environment()->getLoader()->exists($name)) {
+      return $renderer->render($name, $data);
+    }
 
-		$name = $prefix . '/default.html.twig';
+    $name = $prefix . '/default.html.twig';
 
-		return $renderer->render($name, $data);
-	}
+    return $renderer->render($name, $data);
+  }
 }
